f58374692b88b902d568d9acf0d361c960ced707
Stu Leach <jstuartleach@gmail.com>
2022-05-02 18:28:14 -0400

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script type="ts">
+	import ImageRow from './../components/ImageRow.svelte';
+	import Headshot from './../components/Headshot.svelte';
+	import Header from './../components/Header.svelte';
+</script>
+
+<ImageRow />
+<Headshot />
+
+<Header />
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
